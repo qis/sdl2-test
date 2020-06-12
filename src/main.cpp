b105d96a4a064594a8ce73137c4a46729e450003
Alexej Harm <alexej.h@xiphos.de>
2020-06-12 07:43:43 +0200

    added success message

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,12 @@ int main(int argc, char* argv[]) {
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
       throw std::runtime_error(SDL_GetError());
     }
+    std::puts("SDL2 initialized");
   }
   catch (const std::exception& e) {
     std::fputs(e.what(), stderr);
     std::fputc('\n', stderr);
+    std::fflush(stderr);
     return EXIT_FAILURE;
   }
   return EXIT_SUCCESS;
